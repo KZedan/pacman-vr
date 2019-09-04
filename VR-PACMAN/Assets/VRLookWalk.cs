6a58b3dc969986c2030e0fc2a1ecfa6fdec321ba
Student <student@Makerss-MacBook-Pro.local>
2019-09-04 14:55:59 +0100

    trump audio plus teleporting script

--- VR-PACMAN/Assets/VRLookWalk.cs
+++ VR-PACMAN/Assets/VRLookWalk.cs
@@ -6,24 +6,31 @@ public class VRLookWalk : MonoBehaviour
 {
     public Transform vrCamera;
 
-    public float toggleAngle = 30.0f;
+    public float toggleAngle = 0.0f;
 
     public float speed = 2.0f;
 
     public bool moveForward;
+
+    public bool camAng;
+
+    //Camera  
+
+    //Application.LoadLevel("scene1"); camera.position = (4,20,5);
 
     private CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
     {
+
         cc = GetComponent<CharacterController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveForward = true;
+       moveForward = vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f ? true : false;
 
         if (moveForward)
         {
@@ -31,5 +38,7 @@ public class VRLookWalk : MonoBehaviour
 
             cc.SimpleMove(forward * speed);
         }
+
+   
     }
 }
