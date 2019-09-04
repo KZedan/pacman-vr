b1ee7a2171120c9c68e899df7b7a53bd53a55bac
iAquino47 <ianaquino47@gmail.com>
2019-09-04 14:51:49 +0100

    added teleporting doors

--- VR-PACMAN/Assets/VRLookWalk.cs
+++ VR-PACMAN/Assets/VRLookWalk.cs
@@ -6,25 +6,30 @@ public class VRLookWalk : MonoBehaviour
 {
     public Transform vrCamera;
 
-    public float toggleAngle = 30.0f;
+    public float toggleAngle = 0.0f;
 
-    public float speed = 2.0f;
+    public float speed = 3.0f;
 
     public bool moveForward;
 
     private CharacterController cc;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         cc = GetComponent<CharacterController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveForward = true;
-
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f) {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+        
         if (moveForward)
         {
             Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
